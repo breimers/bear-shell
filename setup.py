66b4f12979e478115a2069f69d1cb589c76b43fb
breimers <b.a.reimers@gmail.com>
2025-04-10 16:35:22 -0700

    fixing publish

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bearshell',
-    version='0.1.1',
+    version='0.1.2',
     author='Bradley Reimers',
     author_email='b.a.reimers@gmail.com',
     description='A (relatively) secure and easy-to-use subprocess implementation for Python',
